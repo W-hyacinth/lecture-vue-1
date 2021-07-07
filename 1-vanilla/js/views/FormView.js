faf4d9ac96c9b657cfb552961e2aea81d9b8f9e4
MostOneBrush <w_hyacinth.choi@kakao.com>
2021-07-07 14:35:21 +0900

    실습 UI 개발로 배워보는 순수 javascript 와 VueJS 개발 - 검색폼 구현 4

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -7,7 +7,7 @@ const FormView = Object.create(View)
 FormView.setup = function (el) {
   this.init(el)
   this.inputEl = el.querySelector('[type=text]')
-  this.resetEl = el.querySelector('[type=reset')
+  this.resetEl = el.querySelector('[type=reset]')
   this.showResetBtn(false)
   this.bindEvents()
   return this
@@ -20,13 +20,20 @@ FormView.showResetBtn = function (show = true) {
 FormView.bindEvents = function () {
   this.on('submit', e => e.preventDefault())
   this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+  this.resetEl.addEventListener('click', e => this.onClickReset())
 }
 
 FormView.onKeyup = function (e) {
   const enter = 13
   this.showResetBtn(this.inputEl.value.length)
+  if (!this.inputEl.value.length) this.emit('@reset')
   if (e.keyCode !== enter) return
   this.emit('@submit', { input: this.inputEl.value })
+}
+
+FormView.onClickReset = function () {
+  this.emit('@reset')
+  this.showResetBtn(false)
 }
 
 export default FormView
