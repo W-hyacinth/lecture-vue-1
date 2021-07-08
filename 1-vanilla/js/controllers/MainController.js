a81e41c248817b85ca9a825a7b2f21d92e5de137
MostOneBrush <w_hyacinth.choi@kakao.com>
2021-07-08 22:20:16 +0900

    실습 UI 개발로 배워보는 순수 javascript 와 VueJS 개발 - 추천 검색어 구현 1

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,7 +1,9 @@
 import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
 import TabView from '../views/TabView.js'
+import KeywordView from '../views/KeywordView.js'
 
+import KeywordModel from '../models/KeywordModel.js'
 import SearchModel from '../models/SearchModel.js'
 
 const tag = '[MainController]'
@@ -15,6 +17,7 @@ export default {
     TabView.setup(document.querySelector('#tabs'))
       .on('@change', e => this.onChangeTab(e.detail.tabName))
 
+    KeywordView.setup(document.querySelector('#search-keyword'))
     ResultView.setup(document.querySelector('#search-result'))
 
     this.selectedTab = '추천 검색어'
@@ -22,12 +25,23 @@ export default {
   },
 
   renderView() {
-    console.log(tag, 'rednerView()')
+    console.log(tag, 'renderView()')
     TabView.setActiveTab(this.selectedTab)
+
+    if (this.selectedTab === '추천 검색어') {
+      this.fetchSearchKeyword()
+    } else {
+
+    }
 
     ResultView.hide()
   },
 
+  fetchSearchKeyword() {
+    KeywordModel.list().then(data => {
+      KeywordView.render(data)
+    })
+  },
   search(query) {
     SearchModel.list(query).then(data => {
       this.onSearchResult(data)
